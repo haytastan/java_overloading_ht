3d00ffcaf37a8c1492f690c42a748fc2c6d6c133
hayyam <haytastan@gmail.com>
2022-10-08 10:17:58 +0300

    Java overloading v1.0 (by Hayati TASTAN)

---
+++ src/com/hayatitastan/DortIslem.java
@@ -0,0 +1,5 @@
+package com.hayatitastan;/** 
+ * Created by Dr. Hayati TASTAN on 4.10.2022
+**/
+  public class DortIslem {
+}
---
+++ src/com/hayatitastan/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Dr. Hayati TASTAN on $DATE
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
