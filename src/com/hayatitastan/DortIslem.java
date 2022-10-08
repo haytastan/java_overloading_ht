53286dd0c5b60d4e659e787ca19a15e2963e449a
hayyam <haytastan@gmail.com>
2022-10-08 10:28:48 +0300

    Java overloading v1.0

--- src/com/hayatitastan/DortIslem.java
+++ src/com/hayatitastan/DortIslem.java
@@ -1,5 +1,34 @@
-package com.hayatitastan;/** 
+package com.hayatitastan;
+
+/**
  * Created by Dr. Hayati TASTAN on 4.10.2022
-**/
-  public class DortIslem {
+ **/
+
+public class DortIslem {
+
+  public double Topla(double sayi1, double sayi2){
+    return sayi1 + sayi2;
+  }
+  //the method above is overloaded by the method below:
+  public double Topla(double sayi1, double sayi2, double sayi3){
+    return sayi1 + sayi2 + sayi3;
+  }
+
+  public double Cikar(double sayi1, double sayi2){
+    return sayi1 - sayi2;
+  }
+
+  public double Carp(double sayi1, double sayi2){
+    return sayi1 * sayi2;
+  }
+
+  public double Bol(double sayi1, double sayi2){
+    if (sayi2 != 0) {
+      return sayi1 / sayi2;
+    } else {
+      System.out.println("Sıfıra Bölme Yapılamaz");
+      return 0;
+    }
+  }
+
 }
--- src/com/hayatitastan/Main.java
+++ src/com/hayatitastan/Main.java
@@ -1,8 +1,26 @@
+package com.hayatitastan;
+
 /**
- * Created by Dr. Hayati TASTAN on $DATE
+ * Created by Dr. Hayati TASTAN on 4.10.2022
  **/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DortIslem dortIslem = new DortIslem();
+        double a = 10;
+        double b = 5;
+        double c = 2;
+        double toplamaSonucu2 = dortIslem.Topla(a,b);
+        double toplamaSonucu3 = dortIslem.Topla(a,b,c);
+        double cikarmaSonucu = dortIslem.Cikar(a,b);
+        double carpmaSonucu = dortIslem.Cikar(a,b);
+        double bolmeSonucu = dortIslem.Bol(a,b);
+
+        System.out.println(a + " + " + b + " = " + toplamaSonucu2);
+        System.out.println(a + " + " + b + " + " + c + " = " + toplamaSonucu3);
+        System.out.println(a + " - " + b + " = " + cikarmaSonucu);
+        System.out.println(a + " x " + b + " = " + carpmaSonucu);
+        System.out.println(a + " / " + b + " = " + bolmeSonucu);
+
     }
 }
